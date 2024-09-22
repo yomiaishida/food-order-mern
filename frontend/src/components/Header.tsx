56b21dcab8afd60ca1f77fc08b8767a13a5bf3f1
Abayomi Aishida <yomicorperate@gmail.com>
2024-09-22 01:49:42 +0100

    Main navigation

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 function Header() {
   return (
@@ -13,6 +14,9 @@ function Header() {
         </Link>
         <div className="md:hidden">
           <MobileNav />
+        </div>
+        <div className="hidden md:block">
+          <MainNav />
         </div>
       </div>
     </div>
