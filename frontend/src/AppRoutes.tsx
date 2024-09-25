0df56b6d8c889dcc15240754384f01ba9d55aa82
Abayomi Aishida <yomicorperate@gmail.com>
2024-09-25 18:53:52 +0100

    Protect user route

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoutes from "./auth/ProtectedRoutes";
 
 const AppRoutes = () => {
   return (
@@ -16,14 +17,17 @@ const AppRoutes = () => {
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route
-        path="/user-profile"
-        element={
-          <Layout>
-            <UserProfilePage />
-          </Layout>
-        }
-      />
+
+      <Route element={<ProtectedRoutes />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+      </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
