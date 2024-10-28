006fa1fe22a25ca5c76a940389791087e8a17a88
Abayomi Aishida <yomicorperate@gmail.com>
2024-10-28 22:04:31 +0100

    render loading state while fetching restaurant details

--- frontend/src/auth/ProtectedRoutes.tsx
+++ frontend/src/auth/ProtectedRoutes.tsx
@@ -2,9 +2,21 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoutes = () => {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
+  if (isLoading) {
+    return (
+      <h1 className="h-screen flex justify-center items-center text-4xl text-orange-500">
+        Loading...
+      </h1>
+    );
+  }
+
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
+
+  return <Navigate to="/" replace />;
 };
 
 export default ProtectedRoutes;
